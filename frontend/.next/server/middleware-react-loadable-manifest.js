36c3009b50a6371205e01e02fd7d777fcba488d6
Dylan Payne <dylan@pearledison.com>
2025-04-09 16:21:48 -0600

    Update .gitignore and remove tracked build artifacts

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> next/script\":{\"id\":\"app/page.tsx -> next/script\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_next_dist_api_script_js.js\"]}}"
